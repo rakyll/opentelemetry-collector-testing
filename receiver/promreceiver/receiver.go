91c768c1122103969d084969ecb77b09a35207d5
JBD <jbd@amazon.com>
2020-11-06 18:31:06 -0800

    Changes

--- receiver/promreceiver/receiver.go
+++ receiver/promreceiver/receiver.go
@@ -17,10 +17,10 @@ type reciever struct {
 
 	cancelFunc context.CancelFunc // cancels the prometheus discovery
 
-	logger *zap.Logger
-
 	startOnce sync.Once
 	stopOnce  sync.Once
+
+	logger *zap.Logger
 }
 
 func newReciever(logger *zap.Logger, cfg *Config, consumer consumer.MetricsConsumer) *reciever {
@@ -39,12 +39,6 @@ func (r *reciever) Start(ctx context.Context, host component.Host) error {
 	var startErr error
 	// TODO(jbd): Make logger happen.
 	r.startOnce.Do(func() {
-		scrapeManager := scrape.NewManager(nil, &collector{})
-		if err := scrapeManager.ApplyConfig(r.cfg.PrometheusConfig); err != nil {
-			startErr = err
-			return
-		}
-
 		discoveryManager := discovery.NewManager(discoveryCtx, nil)
 		discoveryCfg := make(map[string]discovery.Configs)
 		for _, scrapeConfig := range r.cfg.PrometheusConfig.ScrapeConfigs {
@@ -53,26 +47,24 @@ func (r *reciever) Start(ctx context.Context, host component.Host) error {
 		if err := discoveryManager.ApplyConfig(discoveryCfg); err != nil {
 			host.ReportFatalError(err)
 		}
+		go func() {
+			if err := discoveryManager.Run(); err != nil {
+				host.ReportFatalError(err) // TODO(jbd): Should halt or just report?
+			}
+		}()
 
-		<-r.runDiscovery(host, discoveryManager)
-		scrapeManager.Run(discoveryManager.SyncCh()) // TODO(jbd): Fix error handling.
-	})
-	return startErr
-}
-
-func (r *reciever) runDiscovery(host component.Host, discoveryManager *discovery.Manager) <-chan struct{} {
-	ch := make(chan struct{})
-	go func() {
-		if err := discoveryManager.Run(); err != nil {
+		scrapeManager := scrape.NewManager(nil, &collector{
+			sink: r.consumer,
+		})
+		if err := scrapeManager.ApplyConfig(r.cfg.PrometheusConfig); err != nil {
+			startErr = err // TODO(jbd): Should halt or just report?
+			return
+		}
+		if err := scrapeManager.Run(discoveryManager.SyncCh()); err != nil {
 			host.ReportFatalError(err) // TODO(jbd): Should halt or just report?
 		}
-	}()
-	ch <- struct{}{}
-	return ch
-}
-
-func (r *reciever) runScraper() {
-
+	})
+	return startErr
 }
 
 func (r *reciever) Shutdown(ctx context.Context) error {
--- receiver/promreceiver/tx.go
+++ receiver/promreceiver/tx.go
@@ -3,35 +3,61 @@ package promreceiver
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/storage"
+	"go.opentelemetry.io/collector/consumer"
+
+	commonpb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
+	resourcepb "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 )
 
-type collector struct{}
+var txidSeq int64
 
-func (c *collector) Appender(ctx context.Context) storage.Appender {
-	return &tx{}
+type collector struct {
+	sink consumer.MetricsConsumer
 }
 
-type tx struct{}
+func (c *collector) Appender(ctx context.Context) storage.Appender {
+	return &tx{
+		txid: atomic.AddInt64(&txidSeq, 1),
+		sink: c.sink,
+	}
+}
 
-func (a *tx) Add(l labels.Labels, t int64, v float64) (uint64, error) {
-	fmt.Println("-----", l, t, v)
+type value struct {
+	l labels.Labels
+	t int64
+	v float64
+}
+
+type tx struct {
+	txid int64 // access atomically
+	sink consumer.MetricsConsumer
+
+	values []*value
+
+	node     *commonpb.Node
+	resource *resourcepb.Resource
+}
+
+func (tt *tx) Add(l labels.Labels, t int64, v float64) (uint64, error) {
+	tt.values = append(tt.values, &value{l: l, t: t, v: v})
 	return 0, nil
 }
 
-func (a *tx) AddFast(ref uint64, t int64, v float64) error {
-	fmt.Println(ref, t, v)
+func (tt *tx) AddFast(ref uint64, t int64, v float64) error {
+	return storage.ErrNotFound
+}
+
+func (tt *tx) Commit() error {
+	for _, v := range tt.values {
+		fmt.Println("----", v)
+	}
 	return nil
 }
 
-func (a *tx) Commit() error {
-	fmt.Println("commit")
-	return nil
-}
-
-func (a *tx) Rollback() error {
-	fmt.Println("rollback")
-	return nil
+func (tt *tx) Rollback() error {
+	return nil // noop
 }
