a055541842fbef223fb3bd53af9feb12092a3066
JBD <jbd@amazon.com>
2020-11-08 16:26:26 -0800

    Changes

--- push.sh
+++ push.sh
@@ -6,8 +6,7 @@ docker push rakyll/otelcol:latest
 
 # docker tag otelcol:latest 516699956539.dkr.ecr.us-east-1.amazonaws.com/otelcol:latest
 # docker push 516699956539.dkr.ecr.us-east-1.amazonaws.com/otelcol:latest
-kubectl apply -f docker-config.yaml
 
-kubectl delete service otel-collector
-kubectl rollout restart deployment otel-collector 
+kubectl delete deployment otel-collector
+kubectl apply -f docker-config.yaml
 kubectl get pods
